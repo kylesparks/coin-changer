6df5ff46cb6d99a7066d81af9298df04991c671c
Kyle Sparks <ksparks@8thlight.com>
2017-08-07 21:06:53 -0500

    attempt 2 - august 7

--- lib/coin_charger.rb
+++ lib/coin_charger.rb
@@ -1,18 +1,19 @@
 class CoinChanger
-  def give_me_my_change(amount)
+  def give_me_my_money(amount)
     change = []
-    coins = [10, 5, 1]
+    coins = [25, 10, 5, 1]
     
     coins.each do |coin|
-      until amount < coin
-        change << coin
-        amount = amount - coin  
-      end 
+    until amount < coin
+      change << coin
+      amount = amount - coin
     end
+  end
     change
   end
 end
 
-# changer = CoinChanger.new
-# changer.give_me_my_change(11)
-# [1, 10]
+#CoinChanger(1) => [1]
+#CoinChanger(25) => [25]
+#CoinChanger(15) => [10, 5]
+#CoinChanger(99) => [25, 25, 25, 10, 10, 1, 1, 1, 1]
--- spec/coin_charger_spec.rb
+++ spec/coin_charger_spec.rb
@@ -1,30 +1,31 @@
 require "coin_charger.rb"
 
-describe CoinChanger do
-  context '#give_me_my_change' do
-    changer = CoinChanger.new
-    it "returns a penny" do
-      expect(changer.give_me_my_change(1)).to eq([1]) 
-    end
-    
-    it "returns two pennies" do
-      expect(changer.give_me_my_change(2)).to eq([1,1])
-    end
-    
-    it "returns a nickel" do
-      expect(changer.give_me_my_change(5)).to eq([5])
-    end
-    
-    it "returns seven cents" do
-      expect(changer.give_me_my_change(7)).to eq([5,1,1])
-    end
-    
-    it "returns a dime" do
-      expect(changer.give_me_my_change(10)).to eq([10])
-    end
-    
-    it "returns 23 cents" do
-      expect(changer.give_me_my_change(23)).to eq([10,10,1,1,1])
-    end
+describe CoinChanger do  
+  it "returns 1 penny for 1 cent" do
+    expect(CoinChanger.new.give_me_my_money(1)).to eq([1])
+  end
+  
+  it "returns 3 pennies for 3 cents" do
+    expect(CoinChanger.new.give_me_my_money(3)).to eq([1, 1, 1])
+  end
+  
+  it "returns a nickel for 5 cents" do
+    expect(CoinChanger.new.give_me_my_money(5)).to eq([5])
+  end
+  
+  it "returns a nickel and two pennies for 7 cents" do
+    expect(CoinChanger.new.give_me_my_money(7)).to eq([5, 1, 1])
+  end
+  
+  it "returns a dime for 10 cents" do
+    expect(CoinChanger.new.give_me_my_money(10)).to eq([10])
+  end
+  
+  it "returns a dime, a nickel, and two pennies for 17 cents" do
+    expect(CoinChanger.new.give_me_my_money(17)).to eq([10, 5, 1, 1])
+  end
+  
+  it "returns a quarter for 25 cents" do
+    expect(CoinChanger.new.give_me_my_money(25)).to eq([25])
   end
 end
