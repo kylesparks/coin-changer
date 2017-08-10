0ae7c6e0bde48898a742adaf0b5e3bb61929d298
Kyle Sparks <ksparks@8thlight.com>
2017-08-10 18:43:28 -0500

    thursday aug 10 attempt

--- lib/coin_changer.rb
+++ lib/coin_changer.rb
@@ -1,15 +1,14 @@
 class CoinChanger
-  def cash_back(amount)
+  def change_back(amount)
     change = []
     coins = [25, 10, 5, 1]
-
+    
     coins.each do |coin|
       until amount < coin
         change << coin
         amount = amount - coin
       end
     end
-      
-    change
+change
   end
 end
--- spec/coin_changer_spec.rb
+++ spec/coin_changer_spec.rb
@@ -1,27 +1,31 @@
 require "coin_changer.rb"
 
 describe CoinChanger do
-  it "gives me a penny for one cent" do
-    expect(CoinChanger.new.cash_back(1)).to eq([1])
+  it "gives me a penny when i need a cent" do
+    expect(CoinChanger.new.change_back(1)).to eq([1])
   end
   
-  it "gives me two pennies for two cents" do
-    expect(CoinChanger.new.cash_back(2)).to eq([1, 1])
+  it "gives me 2 pennies when i need two cents" do
+    expect(CoinChanger.new.change_back(2)).to eq([1, 1])
   end
   
-  it "gives me a nickel for five cents" do
-    expect(CoinChanger.new.cash_back(5)).to eq([5])
+  it "gives me a nickel when i need five cents" do
+    expect(CoinChanger.new.change_back(5)).to eq([5])
   end
   
-  it "gives me a nickel and two pennies for seven cents" do
-    expect(CoinChanger.new.cash_back(7)).to eq([5, 1, 1])
+  it "gives me a nickel and two pennies when i need 7 cents" do
+    expect(CoinChanger.new.change_back(7)).to eq([5, 1, 1])
   end
   
-  it "gives me a dime for 10 cents" do
-    expect(CoinChanger.new.cash_back(10)).to eq([10])
+  it "gives me a dime when i need 10 cents" do
+    expect(CoinChanger.new.change_back(10)).to eq([10])
   end
   
-  it "gives me a quarter for 25 cents" do
-    expect(CoinChanger.new.cash_back(25)).to eq([25])
+  it "gives me a quarter when i need 25 cents" do
+    expect(CoinChanger.new.change_back(25)).to eq([25])
+  end
+  
+  it "gives me a quarter, a dime, a nickel, and two pennies when i need 42 cents" do
+    expect(CoinChanger.new.change_back(42)).to eq([25, 10, 5, 1, 1])
   end
 end
